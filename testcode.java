223f403310f7c53daa3c4c0afa95c28be98dde0a
Nayaab-Jindani05 <d2022.nayaab.jindani@ves.ac.in>
2024-09-27 23:49:29 +0530

    Add files via upload

---
+++ testcode.java
@@ -0,0 +1,13 @@
+public class SyntaxErrorExample {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the program") 
+        System.out.print("This line is correct.\n")
+        System.out.print("This line has a missing closing parenthesis";
+        System.out.println("Goodbye!"); 
+        System.out.println("Another line without a semicolon") 
+        int x = 5
+        if (x > 0 {
+            System.out.println("x is positive");
+        }
+    }
+}
